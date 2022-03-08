569ab5179ba58348877a0f32c6f9c5879e218b45
Kamil-T <trzaskomakamil@gmail.com>
2022-03-09 00:12:39 +0100

    calculator works

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -1,23 +1,56 @@
+import { ChangeEvent, SetStateAction, useEffect, useState } from 'react'
 import styles from '../styles/Calculator.module.scss'
 
 const Calculator = () => {
+  const [count, setCount] = useState<string>('')
+  const [price, setPrice] = useState<string>('')
+  const [saving, setSaving] = useState<number>(NaN)
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    setFunction: { (value: SetStateAction<string>): void }
+  ) => {
+    setFunction(e.target.value)
+  }
+
+  useEffect(() => {
+    let countNumber: string | number = parseFloat(count)
+    let priceNumber: string | number = parseFloat(price)
+    if (count !== undefined && price !== undefined) {
+      setSaving(countNumber * priceNumber - (countNumber * priceNumber) / 5)
+    }
+  }, [count, price])
+
   return (
     <div id={styles.calculator}>
       <p>Ile zaoszczędzisz?</p>
       <span>Rachunek jest prosty</span>
       <p>Ile pojemników na szkło zapełniasz miesięcznie?</p>
       <form>
-        <input type='text' name='container-count' placeholder='20' />
+        <input
+          type='text'
+          name='container-count'
+          placeholder='20'
+          onChange={(e) => handleChange(e, setCount)}
+        />
       </form>
       <p>Ile płacisz za wywóz jednego pojemnika na szkło?</p>
       <form>
-        <input type='text' name='price-of-container' placeholder='20' />
+        <input
+          type='text'
+          name='price-of-container'
+          placeholder='20'
+          onChange={(e) => handleChange(e, setPrice)}
+        />
       </form>
 
-      <div>
-        <p>
-          Dzięki kruszarce zaoszczędzisz <span>900</span> rocznie
+      <div className={styles.saving}>
+        <span>Dzięki kruszarce zaoszczędzisz</span>
+        <p id={isNaN(saving) === false ? styles.active : ''}>
+          {' '}
+          {isNaN(saving) === true ? '900' : saving} zł{' '}
         </p>
+        <span>rocznie.</span>
       </div>
     </div>
   )
